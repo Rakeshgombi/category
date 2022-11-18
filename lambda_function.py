026426f2f4691e96ddd1fd083c92567f1c622f31
Rakesh Gombi <rakeshgombi18@gmail.com>
2022-11-18 15:48:27 +0530

    updated lambda_functions.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,11 +4,11 @@ import test
 
 app = FastAPI(title='Category')
 
+
 @app.get("/", tags=["default"])
 async def get_root():
-    return {"message" : "Hello world"}
-    
+    return {"message": "Hello Vidhi"}
+
 
 app.include_router(test.router)
 lambda_handler = Mangum(app=app)
-
